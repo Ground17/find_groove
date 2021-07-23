b223bf9bfc2570d664c511b0ab4778a72e28b299
Jisang-dev <51708677+Jisang-dev@users.noreply.github.com>
2021-07-23 15:42:10 +0900

    Unity init file upload

---
+++ 2ndAlgorithm/Algorithms.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Algorithms : MonoBehaviour
+{
+    public static void FFT() {
+
+    }
+
+    public static void Horspool()
+    {
+
+    }
+}
