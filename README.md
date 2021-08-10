f48b818a6acda3c258443d3c6a1f3aba5a549f98
jisang-dev <hyla981020@gmail.com>
2021-08-10 21:54:24 +0900

    코드, 파일 구조 수정

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # senier-project
 졸업작품
 
+python/ : 스펙토그램 생성, 
+csharp/ : for Unity
+data : 음악 등
+
 hylu-s pw:1595
---
+++ python/mp3towav.py
@@ -0,0 +1,18 @@
+from pydub import AudioSegment
+import os
+
+def mp3towav():
+    path = '../data/input_mp3/'
+    music_list = os.listdir(path)
+    for k in music_list:
+
+        # files                                                                       
+        src = '../data/input_mp3/'+k
+        dst = '../data/output_wav/'+k.replace('.mp3','.wav')
+
+        # convert mp3 to wav
+        # 사용 전 ffmpeg 다운로드, 환경변수 설정                                                         
+        audSeg = AudioSegment.from_mp3(src)
+        audSeg.export(dst, format="wav")
+
+mp3towav()
